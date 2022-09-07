d6696613b8e70d3a5b6c8706b6bf0f71170265c7
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-09-08 00:58:58 +0300

    cameras

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import type {NextPage} from 'next'
 import {Canvas} from "@react-three/fiber";
 import AnimatedBox from "../components/AnimatedBox";
-import {OrbitControls, Stats} from "@react-three/drei";
+import {OrbitControls, Stats, PerspectiveCamera} from "@react-three/drei";
 
 const Home: NextPage = () => {
     const testing = true;
 
     return (
         <div className="container">
-            <Canvas>
+            <Canvas orthographic camera={{left: -5, top: 5, right: 5, bottom: -5, zoom: 50}}>
+                {/*<PerspectiveCamera makeDefault/>*/}
                 {testing ? <Stats/> : null}
                 {testing ? <axesHelper args={[2]}/> : null}
                 {testing ? <gridHelper args={[10, 10]}/> : null}
