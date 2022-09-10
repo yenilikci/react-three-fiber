b33e8422dfdcee9847b0920ed6a12556da7c6681
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-09-10 13:24:17 +0300

    materials

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,34 @@
 import type {NextPage} from 'next'
 import {Canvas} from "@react-three/fiber";
-import AnimatedBox from "../components/AnimatedBox";
-import {OrbitControls, Stats, PerspectiveCamera, Torus} from "@react-three/drei";
+import {OrbitControls, Stats, useTexture} from "@react-three/drei";
 import React from "react";
+
+const TexturedShapes = () => {
+    const map = useTexture("./textures/metal_plate_diff_1k.png")
+    const displacementMap = useTexture("./textures/metal_plate_disp_1k.png")
+    const normalMap = useTexture("./textures/metal_plate_nor_gl_1k.png")
+    const roughnessMap = useTexture("./textures/metal_plate_rough_1k.png")
+
+    return (
+        <>
+            <mesh scale={[0.5, 0.5, 0.5]} position={[-1.5, 0, 0]}>
+                <sphereGeometry/>
+                <meshStandardMaterial map={map}/>
+            </mesh>
+            <mesh scale={[0.5, 0.5, 0.5]} position={[0, 0, 0]}>
+                <sphereGeometry/>
+                <meshStandardMaterial map={map} normalMap={normalMap} roughnessMap={roughnessMap}/>
+            </mesh>
+            <mesh scale={[0.5, 0.5, 0.5]} position={[1.5, 0, 0]}>
+                <sphereGeometry args={[1, 200, 200]}/>
+                <meshStandardMaterial
+                    map={map} normalMap={normalMap} roughnessMap={roughnessMap} displacementMap={displacementMap}
+                    displacementScale={0.05}
+                />
+            </mesh>
+        </>
+    )
+}
 
 const Home: NextPage = () => {
     const testing = true;
@@ -16,14 +42,9 @@ const Home: NextPage = () => {
                 {testing ? <gridHelper args={[10, 10]}/> : null}
                 <OrbitControls/>
                 <Stats/>
-                <ambientLight intensity={0.1}/>
-                <directionalLight position={[0, 0, 5]}/>
-                <mesh scale={[0.5, 0.5, 0.5]}>
-                    {/*<boxGeometry/>*/}
-                    <coneGeometry/>
-                    <Torus/>
-                    <meshStandardMaterial color={"blue"} wireframe={true}/>
-                </mesh>
+                <ambientLight intensity={0.3}/>
+                <directionalLight position={[0, 5, 5]}/>
+                <TexturedShapes/>
             </Canvas>
         </div>
     )
