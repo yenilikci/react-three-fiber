e5e6046e7f516a402bf1cc71443cbf132a1977d5
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-09-10 12:31:56 +0300

    geometries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type {NextPage} from 'next'
 import {Canvas} from "@react-three/fiber";
 import AnimatedBox from "../components/AnimatedBox";
-import {OrbitControls, Stats, PerspectiveCamera} from "@react-three/drei";
+import {OrbitControls, Stats, PerspectiveCamera, Torus} from "@react-three/drei";
+import React from "react";
 
 const Home: NextPage = () => {
     const testing = true;
@@ -16,8 +17,13 @@ const Home: NextPage = () => {
                 <OrbitControls/>
                 <Stats/>
                 <ambientLight intensity={0.1}/>
-                <directionalLight color="red" position={[0, 0, 5]}/>
-                <AnimatedBox isTesting={testing}/>
+                <directionalLight position={[0, 0, 5]}/>
+                <mesh scale={[0.5, 0.5, 0.5]}>
+                    {/*<boxGeometry/>*/}
+                    <coneGeometry/>
+                    <Torus/>
+                    <meshStandardMaterial color={"blue"} wireframe={true}/>
+                </mesh>
             </Canvas>
         </div>
     )
